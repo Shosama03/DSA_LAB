8da60b248de4b52f7512d56ffe14b669a9cfa3e0
Rajat Singh <rajatsingh20060@gmail.com>
2025-09-16 18:36:52 +0530

    Intersection_Operation

--- Set_Operations/Intersection_Operation.cpp
+++ Set_Operations/Intersection_Operation.cpp
@@ -2,63 +2,54 @@
 #include <vector>
 using namespace std;
 
+// Function to compute intersection of two sorted arrays
+vector<int> intersection(const vector<int>& a, const vector<int>& b);
 
-vector<int> intersection(const vector<int>&a,const vector<int>&b);
-int main()
-{
+int main() {
     vector<int> a;
     vector<int> b;
     int val;
 
-    int n1,n2;
-    cout<<"Enter the size of Set A"<<endl;
-    cin>>n1;
-    cout<<"Enter the elements of Set A"<<endl;
-
-    for (int i=0; i<n1; i++)
-    {
-        
-        cin>>val;
+    int n1, n2;
+    cout << "Enter the size of Set A: ";
+    cin >> n1;
+    cout << "Enter the elements of Set A (sorted): ";
+    for (int i = 0; i < n1; i++) {
+        cin >> val;
         a.push_back(val);
     }
 
-    cout<<"Enter the size of Set B."<<endl;
-    cin>>n2;
-    cout<<"Enter the elements of Set B:"<<endl;
-    for(int i=0;i<n2;i++)
-    {
-        cin>>val;
+    cout << "Enter the size of Set B: ";
+    cin >> n2;
+    cout << "Enter the elements of Set B (sorted): ";
+    for (int i = 0; i < n2; i++) {
+        cin >> val;
         b.push_back(val);
     }
 
-    //Taking the union of both the sets.
-    cout<<"The Intersection of the Array is "<<endl;
-    vector<int> c = intersection(a,b);
-    
-    for(int x:c)
-    {
-        cout<<x<<" ";
+    vector<int> c = intersection(a, b);
+    cout << "The Intersection of the Arrays is: ";
+    for (int x : c) {
+        cout << x << " ";
     }
-
+    cout << endl;
 }
 
-vector<int> intersection(const vector<int>&a,const vector<int>&b)
-{
-    vector <int> result;
-    int i=0,j=0;
-    while(i<a.size() && j<b.size())
-    {
-        if (a[i]==b[j])
-        {
-            result.push_back(a[i++]);
+vector<int> intersection(const vector<int>& a, const vector<int>& b) {
+    vector<int> result;
+    int i = 0, j = 0;
+
+    while (i < a.size() && j < b.size()) {
+        if (a[i] == b[j]) {
+            result.push_back(a[i]);
+            i++;
+            j++;
+        } else if (a[i] < b[j]) {
+            i++;
+        } else {
             j++;
         }
-        else
-        continue;
-        
-        
     }
+
     return result;
 }
-
-
